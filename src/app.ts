3b7c7db99975afec5a99f0531bda395da8e36324
Elber Lopes <elbercaio@gmail.com>
2021-10-18 20:04:03 -0300

    Rota de login e de callback

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
+import "dotenv/config"
 import express from "express";
 
 const app = express(); // criar app 
 
-app.listen(4000, () => console.log('👌 Server is runnning on port 4000'))
-
+app.get('/github', (request, response) => {
+    response.redirect(
+        `https://github.com/login/oauth/authorize?client_id=${process.env.
+        GITHUB_CLIENT_ID}`)
+})
+app.get('/signin/callback', (request, response) => {
+    const { code } = request.query; //desestruturar codigo de acesso
+    
+    return response.json(code);
+})
+app.listen(4000, () => console.log(
+    '👌 Server is runnning on port 4000'))
